0f8c08df61e576ee44666e387fee73b6b2e49400
Andy Kenward <andy@kenruff.co.uk>
2016-03-30 20:44:35 +0100

    trial version with improved logic at end of form sequence

--- referral.js
+++ referral.js
@@ -6,9 +6,15 @@
 Drupal.behaviors.referral_remove_tabledrag_toggle = {
   attach: function (context, settings) {
     (function ($) {
-    // All your code here
+    // Clear the 'show row weights' and 'cardinality count' messages
       $('.tabledrag-toggle-weight').text(Drupal. t(''));
       $('.ief-cardinality-count').html('');
+      if (Drupal.settings.referral.subformFlag) {
+        $('.form-actions').hide()
+      }
+      else {
+      	$('.form-actions').show()
+      }
     }(jQuery));
   }
 };
